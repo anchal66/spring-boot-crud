7b8553a9101ee5995ca658bfbfdbc6edeabc5bd3
Avinash Anchal <you@example.comanchalavinash66@gmail.com>
2020-01-18 12:18:49 +0530

    First Commit

---
+++ springdemoproj/src/main/java/com/anchal/springdemoproj/SpringdemoprojApplication.java
@@ -0,0 +1,13 @@
+package com.anchal.springdemoproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdemoprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdemoprojApplication.class, args);
+	}
+
+}
